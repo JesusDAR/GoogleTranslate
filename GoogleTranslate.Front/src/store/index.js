ea8fcff5cbb6e422258baad42cfd2b1f24567df6
Jesus <jesus.rodrigo@samelan.com>
2022-01-02 01:51:05 +0100

    env

--- GoogleTranslate.Front/src/store/index.js
+++ GoogleTranslate.Front/src/store/index.js
@@ -11,7 +11,7 @@ export default new Vuex.Store({
     textSrc: '',
     textTgt: '',
     url: 'https://localhost:44304/api',
-    url_dev: 'http://localhost:5000/api'
+    url_prod: 'https://localhost:5000/api'
   },
   mutations: {
     setSrc(state, src) {
@@ -32,7 +32,7 @@ export default new Vuex.Store({
     translate(context){
       let api = ''
       if (process.env.NODE_ENV !== 'development'){
-        api = this.state.url_dev + '/Translation'
+        api = this.state.url_prod + '/Translation'
       }
       else{
         api = this.state.url + '/Translation'
